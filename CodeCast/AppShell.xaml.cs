9bf6b9421a77dc179c5d599a31de69c963209468
ZairiAimenDz <zairiaimendev@gmail.com>
2022-07-16 08:06:24 +0100

    Add project files.

---
+++ CodeCast/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeCast;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CodeCast/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CodeCast;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
